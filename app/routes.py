ba9df694c4c88c3cf34ccc307dcf9c1d23002a9e
Andrii Kreidun <akreidun@MacBook-Pro-Andrii.local>
2020-07-14 02:38:18 +0300

    Adding posts to the blog

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,23 @@
-from app import app
+# -*- coding: utf-8 -*-
 from flask import render_template
-
+from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Andrii'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
